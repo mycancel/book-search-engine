e2f0485fbdd525524392b39252b4e45063bd4b13
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-28 14:26:32 -0400

    Added AuthenticationErrors to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,14 +2,20 @@
 const { User } = require("../models");
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
 
 const resolvers = {
   Query: {
     // get a single user by either their id or their username
     getSingleUser: async (parent, { userId, username }) => {
-      return await User.findOne({
-        $or: [{ _id: userId }, { username: username }],
+      const foundUser =  await User.findOne({
+        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
       });
+      if (!foundUser) {
+        throw new AuthenticationError('Cannot find a user with this id!');
+      }
+      return foundUser;
     },
   },
 
@@ -17,46 +23,63 @@ const resolvers = {
     // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
     createUser: async (parent, args) => {
       const user = await User.create(args);
+      if (!user) {
+        throw new AuthenticationError('Something is wrong!');
+      }
       const token = signToken(user);
       return ({ token, user });
     },
     // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-    saveBook: async (
-      parent,
-      { userId, authors, description, bookId, image, link, title }
-    ) => {
-      return await User.findOneAndUpdate(
-        { _id: userId },
-        {
-          $addToSet: {
-            savedBooks: {
-              authors: authors,
-              description: description,
-              bookId: bookId,
-              image: image,
-              link: link,
-              title: title,
+    saveBook: async (parent, args) => {
+      try {
+        return await User.findOneAndUpdate(
+          { _id: args.userId },
+          {
+            $addToSet: {
+              savedBooks: {
+                authors: args.authors,
+                description: args.description,
+                bookId: args.bookId,
+                image: args.image,
+                link: args.link,
+                title: args.title,
+              },
             },
           },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      } catch (err) {
+        console.log(err);
+        throw new AuthenticationError('Something is wrong!');
+      }
     },
     // remove a book from `savedBooks`
     deleteBook: async (parent, { userId, bookId }) => {
-      return await User.findOneAndUpdate(
+      const updatedUser = await User.findOneAndUpdate(
         { _id: userId },
         { $pull: { savedBooks: { bookId: bookId } } },
         { new: true }
       );
+      if (!updatedUser) {
+        throw new AuthenticationError("Couldn't find user with this id!");
+      }
+      return updatedUser;
     },
     // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
     login: async (parent, { username, email, password }) => {
       const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
-      // TODO: Call custom method .isCorrectPassword to validate password.
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+  
+      const correctPw = await user.isCorrectPassword(body.password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!');
+      }
       const token = signToken(user);
       return ({ token, user });
     },
